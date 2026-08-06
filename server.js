b36088be1ae425767b6f3641fbf8cb5d5c141926
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-08-07 02:06:19 +0530

    initialize Node.js resume parser project

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+ 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
